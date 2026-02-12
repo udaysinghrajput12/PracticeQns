3216d3c758d7fc0ad3c74470fcb30170a82dfbd8
udaysinghrajput12 <97505262+udaysinghrajput12@users.noreply.github.com>
2026-02-12 10:24:43 +0530

    Add files via upload

---
+++ Palindrome.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a word:");
+        string word = Console.ReadLine();
+
+        string reversed = "";
+
+        for (int i = word.Length - 1; i >= 0; i--)
+        {
+            reversed += word[i];
+        }
+
+        if (word == reversed)
+            Console.WriteLine("Palindrome");
+        else
+            Console.WriteLine("Not a Palindrome");
+    }
+}
---
+++ Reverse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a sentence:");
+        string input = Console.ReadLine();
+
+        // Split sentence into words
+        string[] words = input.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (i % 2 != 0)  // Alternate words (odd index)
+            {
+                char[] arr = words[i].ToCharArray();
+                Array.Reverse(arr);
+                words[i] = new string(arr);
+            }
+        }
+
+        // Join words back into sentence
+        string result = string.Join(" ", words);
+        Console.WriteLine(result);
+    }
+}
