a948566fb5f4a7d61b638ae1ee6ce37bd062e2fb
udaysinghrajput12 <97505262+udaysinghrajput12@users.noreply.github.com>
2026-03-17 18:05:25 +0530

    Add files via upload

---
+++ task-dashboard-project/frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import TaskDashboard from "./TaskDashboard";
+import ThemeToggle from "./ThemeToggle";
+
+function App() {
+  return (
+    <div>
+      <ThemeToggle />
+      <TaskDashboard />
+    </div>
+  );
+}
+
+export default App;
